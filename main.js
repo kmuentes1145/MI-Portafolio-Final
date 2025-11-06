6695e80e1584ff2b0c261387409654a622ef778f
kmuentes1145 <kmuentes1145@utm.edu.ec>
2025-11-06 10:18:48 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,58 @@
+
+
+$(document).ready(function() {
+
+var rabxbss = $('#inicio').offset().top ;
+$('#btn-inicio').on('click', function() {
+    $('html, body').animate({
+        scrollTop: 30
+    }, 500);
+});
+
+
+var rabxbss = $('#sobremi').offset().top ;
+$('#btn-sobremi').on('click', function() {
+    $('html, body').animate({
+        scrollTop: 590
+    }, 500);
+});
+
+
+var rabxbss = $('#portafolio').offset().top ;
+$('#btn-portafolio').on('click', function() {
+    $('html, body').animate({
+        scrollTop: 1100
+    }, 500);
+});
+
+
+
+
+var rabxbss = $('#experiencia').offset().top ;
+$('#btn-experiencia').on('click', function() {
+    $('html, body').animate({
+        scrollTop: 2000
+    }, 500);
+});
+
+
+
+var rabxbss = $('#formulario').offset().top ;
+$('#btn-formulario').on('click', function() {
+    $('html, body').animate({
+        scrollTop: 3880
+    }, 500);
+});
+
+
+
+var rabxbss = $('#contacto').offset().top ;
+$('#btn-contactame').on('click', function() {
+    $('html, body').animate({
+        scrollTop: 4500
+    }, 500);
+});
+
+
+
+});
